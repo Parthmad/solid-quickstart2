8354e1450475b37cfb2c868afb31d869517440ab
Parthmad <100795861+Parthmad@users.noreply.github.com>
2023-09-28 18:57:16 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import Chatbot from 'flowise-embed';
-
-class MyComponent extends React.Component {
-
-  componentDidMount() {
-    Chatbot.init({
-      chatflowid: '5209eae0-447d-4e73-83db-64f518b187d9',
-      apiHost: 'http://localhost:3000',
-    });
-  }
-
-  render() {
-    const iframeHTML = `<iframe src="https://share.synthesia.io/embeds/videos/25877130-d883-40ce-a996-bfd7bc9c272a" loading="lazy" title="Synthesia video player - Sales Tax Compliance Complexity" allow="encrypted-media; fullscreen;" style="position: absolute; width: 70%; height: 70%; top: 0; left: 0; border: none; padding: 0; margin: 0; overflow:hidden;"></iframe>`;
-    return (
-      <div style={{ position: 'relative', overflow: 'hidden', aspectRatio: '1920/1080' }} dangerouslySetInnerHTML={{ __html: iframeHTML }} />
+async function query(data) {
+    const response = await fetch(
+        "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
     );
-  }
+    const result = await response.json();
+    return result;
 }
 
-export default MyComponent;
+query({"question": "Hey, how are you?"}).then((response) => {
+    console.log(response);
+});
