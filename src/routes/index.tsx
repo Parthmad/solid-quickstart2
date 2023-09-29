323b78c5f692328e8c4831063cbb0bc03e74db35
Parthmad <100795861+Parthmad@users.noreply.github.com>
2023-09-28 19:01:18 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,18 @@
-import fetch from 'node-fetch';
-
-interface Data {
-    question: string;
+async function query(data) {
+  const response = await fetch(
+    "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result;
 }
 
-async function query(data: Data): Promise<any> {
-    const response = await fetch(
-        "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        }
-    );
-    const result = await response.json();
-    return result;
-}
-
-query({"question": "Hey, how are you?"}).then((response) => {
-    console.log(response);
+query({ question: "Hey, how are you?" }).then((response) => {
+  console.log(response);
 });
